1feb3dea2534b2d0c0f527120396990b5a08d3f6
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-11-10 09:35:07 +0800

    微信号号主抓取

---
+++ getAnyv.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+from pymysql import *
+
+'''
+抓取爱妮微网站首页的热门微信号号主信息：http://top.anyv.net/
+'''
+
+def getUser():
+    headers = {
+        'Host': 'top.anyv.net',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.90 Safari/537.36 2345Explorer/9.3.2.17331',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Referer': 'http://www.anyv.net/index.php/category-81',
+    }
+    url = 'http://top.anyv.net/'
+    body = requests.get(url,headers=headers).text
+    response = BeautifulSoup(body,'lxml')
+
+    data = response.select('ul')[3].get_text()
+    data = data.split('\n')
+
+    user = []
+    for i in range(len(data)):
+        if data[i] == '':
+            pass
+        else:
+            user.append(data[i])
+
+    return user
+
+if __name__ == "__main__":
+    db = connect(host="localhost", port=3306, db="spider", user="root", password="123456", charset="utf8")
+    conn = db.cursor()
+
+    user = getUser()
+
+    user_set = set(user)
+    user_list_1 = list(user_set)
+    user_list_1.sort(key=user.index) #去重后按照原来的顺序进行入库
+    print(len(user_list_1))
+
+    for i in range(len(user_list_1)):
+        user_name = user_list_1[i]
+        print(user_name)
+        user_id = 'None'
+        biz = 'None'
+
+        sql = """insert into wechat(user_name,user_id,biz) value(%s,%s,%s)"""
+        try:
+            conn.execute(sql, (user_name,user_id,biz))
+            db.commit()
+        except:
+            db.rollback()
+
+    db.close()
